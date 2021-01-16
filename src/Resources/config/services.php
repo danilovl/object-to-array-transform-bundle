fe0e5e0953c912de2174d2cfc1a0d292c62610dd
Vladimir Danilov <dev.danilovl@gmail.com>
2021-01-16 13:38:44 +0100

    Prepare for Symfony 6. Convert yaml to php configs.

---
+++ src/Resources/config/services.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+use Danilovl\ObjectToArrayTransformBundle\Services\ObjectToArrayTransformService;
+
+return static function (ContainerConfigurator $container): void {
+    $container->services()
+        ->set('danilovl.object_to_array_transform', ObjectToArrayTransformService::class)
+        ->args([
+            service('danilovl.parameter'),
+        ])
+        ->public()
+        ->alias(ObjectToArrayTransformService::class, 'danilovl.object_to_array_transform');
+};
