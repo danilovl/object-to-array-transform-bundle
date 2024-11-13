cc2ed4056989e19323e7964f5e6c4f603ee4cab5
Vladimir Danilov <dev.danilovl@gmail.com>
2024-11-13 20:51:45 +0100

    ObjectToArrayTransformExtension - change Extension class.

--- src/DependencyInjection/ObjectToArrayTransformExtension.php
+++ src/DependencyInjection/ObjectToArrayTransformExtension.php
@@ -3,9 +3,9 @@
 namespace Danilovl\ObjectToArrayTransformBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class ObjectToArrayTransformExtension extends Extension
 {
